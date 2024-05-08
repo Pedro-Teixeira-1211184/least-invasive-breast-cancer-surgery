352fa5728936e6d998a7131655a7a32095f11728
pedrogpteixeira <pgpteixeira9@gmail.com>
2024-05-08 03:50:13 +0100

    deleted other approach

--- mongodb-file-download-tuto-main/api/db/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-
-const connectDB = (url) => {
-  return mongoose.connect(url, {}, console.log("connected to mongodb atlas"));
-};
-
-module.exports = connectDB;
