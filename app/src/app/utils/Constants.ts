4237b54e80270f720fea52b521891c7bc5f80e69
pedrogpteixeira <pgpteixeira9@gmail.com>
2024-05-21 17:16:53 +0100

    remote apis connection

--- app/src/app/utils/Constants.ts
+++ app/src/app/utils/Constants.ts
@@ -1,9 +1,9 @@
 export default class Constants {
   // user authentication
-  //public static readonly API_BASE_URL_USERS = 'https://least-invasive-breast-cancer-surgery.onrender.com/';
-  //public static readonly API_BASE_URL_MODELS = 'https://least-invasive-breast-cancer-surgery-2kt9.onrender.com/';
-  public static readonly API_BASE_URL_USERS = 'http://localhost:3000/';
-  public static readonly API_BASE_URL_MODELS = 'http://localhost:4000/';
+  public static readonly API_BASE_URL_USERS = 'https://least-invasive-breast-cancer-surgery.onrender.com/';
+  public static readonly API_BASE_URL_MODELS = 'https://least-invasive-breast-cancer-surgery-2kt9.onrender.com/';
+  //public static readonly API_BASE_URL_USERS = 'http://localhost:3000/';
+  //public static readonly API_BASE_URL_MODELS = 'http://localhost:4000/';
 
   //user authentication
   public static readonly API_AUTH_LOGIN_URL = Constants.API_BASE_URL_USERS + 'api/auth/signin';
