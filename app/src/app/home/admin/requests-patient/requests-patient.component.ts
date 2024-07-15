cd263ca4ef388588d629f56c21d1189a97b218d6
Pedro Teixeira <1211184@isep.ipp.pt>
2024-07-15 13:44:03 +0100

    corrected error with patient sign up request

--- app/src/app/home/admin/requests-patient/requests-patient.component.ts
+++ app/src/app/home/admin/requests-patient/requests-patient.component.ts
@@ -3,6 +3,8 @@ import {FormsModule} from "@angular/forms";
 import {NgForOf, NgIf} from "@angular/common";
 import {AuthService} from "../../../service/auth/auth.service";
 import {ISignUpRequestPatientDTO} from "../../../dto/ISignUpRequestPatientDTO";
+import IRoleDTO from "../../../dto/IRoleDTO";
+import Constants from "../../../utils/Constants";
 
 @Component({
   selector: 'app-requests-patient',
@@ -20,12 +22,14 @@ export class RequestsPatientComponent implements OnInit {
   auth_service: AuthService = inject(AuthService);
   requests: ISignUpRequestPatientDTO[] = [];
   noRequests: boolean = false;
+  role: IRoleDTO | null = null;
 
   constructor() {
   }
 
   ngOnInit(): void {
     this.getAllRequests();
+    this.getRole();
   }
 
   private async getAllRequests(): Promise<void> {
@@ -33,12 +37,18 @@ export class RequestsPatientComponent implements OnInit {
     this.noRequests = this.requests.length == 0;
   }
 
+  private async getRole(): Promise<void> {
+    let roles = await this.auth_service.getAllRoles();
+    //remove patient role
+    roles = roles.filter(role => role.name == Constants.ROLE_PATIENT);
+    this.role = roles[0];
+  }
+
   public async accept(request: ISignUpRequestPatientDTO): Promise<void> {
-    const role = localStorage.getItem('role');
-    if (role == null) {
+    if (this.role == null) {
       return;
     }
-    await this.auth_service.signUpPatient(request.firstName, request.lastName, request.email, request.password, role, request.sns);
+    await this.auth_service.signUpPatient(request.firstName, request.lastName, request.email, request.password, this.role.id, request.sns);
     //refresh content
     this.requests = [];
     await this.getAllRequests();
